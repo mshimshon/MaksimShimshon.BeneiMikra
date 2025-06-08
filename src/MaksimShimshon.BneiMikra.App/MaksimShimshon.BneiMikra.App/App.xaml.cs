5a61c10f94821832478a06c633ca71879c153d63
m.samson <m.samson@opencodedev.com>
2025-06-08 06:47:50 -0600

    Inital Push Sample

---
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MaksimShimshon.BneiMikra.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "MaksimShimshon.BneiMikra.App" };
+        }
+    }
+}
---
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaksimShimshon.BneiMikra.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
