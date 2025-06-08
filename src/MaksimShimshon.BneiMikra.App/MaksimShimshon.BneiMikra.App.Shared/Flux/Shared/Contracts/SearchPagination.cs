36dbc0b2548c61eb7a8750aa0e5ca69f3e64a48b
m.samson <m.samson@opencodedev.com>
2025-06-08 07:16:41 -0600

    Added Pagination and Serach Result Base Objects

---
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App.Shared/Flux/Shared/Contracts/SearchPagination.cs
@@ -0,0 +1,8 @@
+﻿namespace MaksimShimshon.BneiMikra.App.Shared.Flux.Shared.Contracts;
+public record SearchPagination
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int PAgeCount { get; set; }
+    public int Total { get; set; }
+}
---
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App.Shared/Flux/Shared/Contracts/SearchResult.cs
@@ -0,0 +1,6 @@
+﻿namespace MaksimShimshon.BneiMikra.App.Shared.Flux.Shared.Contracts;
+public record SearchResult<T> where T : class
+{
+    public List<T> Result { get; set; } = new();
+    public SearchPagination Pagination { get; set; } = new();
+}
