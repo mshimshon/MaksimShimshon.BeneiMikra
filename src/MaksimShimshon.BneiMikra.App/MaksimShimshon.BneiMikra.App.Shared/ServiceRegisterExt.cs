d80c150f5a1f71953de07735007ea85a9a80efc6
m.samson <m.samson@opencodedev.com>
2025-06-12 14:03:56 +0300

    wip - fixed some scope issues and add mudblazor and httpclient

---
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App.Shared/ServiceRegisterExt.cs
@@ -0,0 +1,52 @@
+﻿global using Fluxor;
+global using MudBlazor;
+global using MudBlazor.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MaksimShimshon.BneiMikra.App.Shared;
+public static class ServiceRegisterExt
+{
+    public static void AddUIServices(this IServiceCollection serviceDescriptors)
+    {
+        serviceDescriptors.AddMudServices(ConfigureMudService);
+        //        serviceDescriptors.AddScoped<IResourceProvider<AuthenticationResource>, GenericResourceProvider<AuthenticationResource>>();
+        //        serviceDescriptors.AddScoped<IResourceProvider<BetaWarningResource>, GenericResourceProvider<BetaWarningResource>>();
+        //        serviceDescriptors.AddScoped<IResourceProvider<CodedExceptionsResource>, GenericResourceProvider<CodedExceptionsResource>>();
+        //        serviceDescriptors.AddScoped<IResourceProvider<ApplicationResource>, GenericResourceProvider<ApplicationResource>>();
+        //        serviceDescriptors.AddScoped<IResourceProvider<CustomIngredientResource>, GenericResourceProvider<CustomIngredientResource>>();
+        //        serviceDescriptors.AddScoped<IResourceProvider<FormulationResource>, GenericResourceProvider<FormulationResource>>();
+        //        serviceDescriptors.AddScoped<IDispatcherClient, DispatcherClient>();
+        //        serviceDescriptors.AddScoped<IJSProvider, JavaScriptProvider>();
+        //        serviceDescriptors.AddScoped<IEnvironmentProvider, EnvironmentProvider>();
+        //        serviceDescriptors.AddScoped<ILocalStorageProvider, LocalStorageProvider>();
+        //        serviceDescriptors.AddScoped<IStartupProvider, StartupProvider>();
+        //        serviceDescriptors.AddScoped<IHttpClientProvider, HttpClientProvider>();
+        //        serviceDescriptors.RegisterProtocolLinks();
+        serviceDescriptors.AddFluxor(options =>
+        {
+            options.ScanAssemblies(typeof(ServiceRegisterExt).Assembly);
+#if DEBUG
+            //options.UseReduxDevTools(opt =>
+            //{
+
+            //    opt.Name = "My application";
+            //    opt.EnableStackTrace();
+            //});
+#endif
+        });
+        // Register Protocol Commands
+        //serviceDescriptors.AddScoped<ILinkProtocolHandler<AuthenticatedRequest>, AuthenticatedHandler>();
+        serviceDescriptors.AddHttpClient();
+    }
+    private static void ConfigureMudService(MudServicesConfiguration config)
+    {
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+        config.SnackbarConfiguration.PreventDuplicates = true;
+        config.SnackbarConfiguration.NewestOnTop = false;
+        config.SnackbarConfiguration.ShowCloseIcon = true;
+        config.SnackbarConfiguration.VisibleStateDuration = 5000;
+        config.SnackbarConfiguration.HideTransitionDuration = 500;
+        config.SnackbarConfiguration.ShowTransitionDuration = 500;
+        config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+    }
+}
--- src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App/MauiProgram.cs
+++ src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App/MauiProgram.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.Extensions.Logging;
 
-namespace MaksimShimshon.BneiMikra.App
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+namespace MaksimShimshon.BneiMikra.App;
 
-            builder.Services.AddMauiBlazorWebView();
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-            builder.Services.AddBlazorWebViewDeveloperTools();
-            builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
+        var build = builder.Build();
 
-            return builder.Build();
-        }
+        return build;
     }
 }
