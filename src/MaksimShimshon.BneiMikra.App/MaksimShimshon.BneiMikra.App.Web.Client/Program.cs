1ac0b294a11a228ac88c1ac1d3274cd6aa37bb78
m.samson <m.samson@opencodedev.com>
2025-07-11 00:02:24 -0400

    remove blazor app wtf microsoft is doing i have no clue.... added wasm project

--- src/MaksimShimshon.BneiMikra.App/MaksimShimshon.BneiMikra.App.Web.Client/Program.cs
+++
@@ -1,7 +0,0 @@
-using MaksimShimshon.BneiMikra.App.Shared;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddUIServices();
-
-await builder.Build().RunAsync();
